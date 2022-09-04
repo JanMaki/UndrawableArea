40bbf6401e6c4f811ec6b3d1ee7a5b557f527247
JanMaki <44093615+JanMaki@users.noreply.github.com>
2022-09-05 02:10:26 +0900

    Add files via upload

---
+++ UndrawableArea.cs
@@ -0,0 +1,26 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace UndrawableArea
+{
+    /// <summary>
+    /// QvPenで書けないエリア
+    /// </summary>
+    public class UndrawableArea : UdonSharpBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            //名前を確認
+            if (!other.gameObject.name.Equals("InkCollider")) return;
+
+            //親オブジェクトを取得
+            var parent = other.gameObject.transform.parent;
+            
+            //nullチェック
+            if (parent == null) return;
+
+            //線を削除
+            Destroy(parent.gameObject);
+        }
+    }
+}
